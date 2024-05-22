4b6c209ef2362d47e45442189e6da18f8f7bb499
Armando Romero <162987396+mandoromero@users.noreply.github.com>
2024-05-22 23:55:44 +0000

    done

--- src/js/component/trafficLight.jsx
+++ src/js/component/trafficLight.jsx
@@ -4,18 +4,19 @@ import { useState } from "react";
 
 
 const TrafficLight = () => {
-    const[clickLight, setClickLight] = useState("")
+    const [clickLight, setClickLight] = useState ("")
     return (
         <div>
             <div id="pole"></div>
             <div id="container">
-                <div className={clickLight==="red"?"glow red light":"red light"} 
-                    onClick = {clickLight==="red"?()=>setClickLight(""):()=>setClickLight("red") }></div>
-                <div className="yellow-light" onClick = {() => this.useState({clickedLight: 'yellow'})}></div>
-                <div className="green-light" onClick = {() => this.useState({clickedLight: 'green'})}></div>
+                <div className={clickLight === "red" ? "glow-red red light" : "red light"} 
+                    onClick = {clickLight=== "red" ? () => setClickLight("") : () => setClickLight("red") }></div>
+                <div className={clickLight === "yellow" ? "glow-yellow yellow light" : "yellow light"} 
+                    onClick = {clickLight === "yellow" ? () => setClickLight("") : () => setClickLight("yellow") }></div>
+                <div className={clickLight === "green" ? "glow-green green light" : "green light"} 
+                    onClick = {clickLight === "green" ? () => setClickLight("") : () => setClickLight("green") }></div>
             </div>
         </div>
-
     )
 };
 
